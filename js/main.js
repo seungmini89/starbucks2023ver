604693008cb9b6526c13de121297652d18c0fddf
seungmini89 <tmd4505@naver.com>
2023-09-17 02:01:14 +0900

    뱃지 이미지 수정 및 js수정

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ window.addEventListener('scroll', _.throttle(function() {
       display: 'none'
     });
     // 버튼 보이기
-    gsap.to('toTopEl', .2, {
+    gsap.to(toTopEl, .2, {
       x: 0
     });
   } else {
@@ -40,7 +40,7 @@ window.addEventListener('scroll', _.throttle(function() {
       display: 'block'
     });
     // 버튼 숨기기
-    gsap.to('toTopEl', .2, {
+    gsap.to(toTopEl, .2, {
       x: 100
     });
   }
